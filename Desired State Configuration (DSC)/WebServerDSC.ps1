4ba184a4d5f61e3b28120eec8f949a0d08146c9d
Vincent Smith <VinceS22@gmail.com>
2018-09-26 17:04:09 -0400

    Desired State Configuration (DSC) Work

---
+++ Desired State Configuration (DSC)/WebServerDSC.ps1
@@ -0,0 +1,43 @@
+﻿$currentDate = Get-Date
+
+configuration WebServerDSC {
+    Node WebServer
+    {
+        WindowsFeature IIS
+        {
+            Ensure = 'Present'
+            Name = "Web-Server"
+            IncludeAllSubFeature = $true
+        }
+        WindowsFeature HTTPFeatures
+        {
+            Ensure = 'Present'
+            Name = 'Web-Common-Http'
+        }
+        WindowsFeature DotNET45
+        {
+            Ensure = 'Present'
+            Name = 'NET-Framework-45-Features'
+            IncludeAllSubFeature = $true
+        }
+        WindowsFeature WebMgmtTools
+        {
+            Ensure = 'Present'
+            Name = 'Web-Mgmt-Tools'
+            IncludeAllSubFeature = $true 
+        }
+        WindowsFeature Security
+        {
+            Ensure = 'Present'
+            Name = 'Web-Security'
+            IncludeAllSubFeature = $true
+        }
+        File DSCApplyValidation
+        {
+            DestinationPath = 'C:\Users\sysadmin\Desktop\Validation.txt'
+            Ensure = 'Present'
+            Contents = 'DSC has been run on ' + $currentDate
+            
+        }
+    }
+}
